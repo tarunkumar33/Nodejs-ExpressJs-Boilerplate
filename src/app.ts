7ea21f2ef3c81c86dc38609f57615863b7b65c4d
Tarun Kumar <tarun.p@technovert.com>
2023-09-25 15:58:24 +0530

    add express error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+import ApiError from "./utils/ApiError";
+import { errorConverter, errorHandler } from "./middlewares/error";
+
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const httpStatus = require("http-status");
 
 const app = express();
 
@@ -15,5 +19,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // enable cross origin resource sharing
 app.use(cors());
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 export default app;
---
+++ src/middlewares/error.ts
@@ -0,0 +1,35 @@
+import ApiError from '../utils/ApiError';
+import logger from '../lib/logger';
+import config from '../configs/config'
+const mongoose = require("mongoose");
+const httpStatus = require("http-status");
+
+export const errorConverter = (err, req, res, next) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode =
+      error.statusCode || error instanceof mongoose.Error
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || httpStatus[statusCode];
+    error = new ApiError(statusCode, message, err.stack);
+  }
+  next(error);
+};
+
+export const errorHandler = (err, req, res, next) => {
+  let { statusCode, message } = err;
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(config.env === "development" && { stack: err.stack }),
+  };
+
+  if (config.env === "development") {
+    logger.error(err);
+  }
+
+  res.status(statusCode).send(response);
+};
