0f24420d16b6d204462b2caa7dce977baa3c65a3
Tarun Kumar <tarun.p@technovert.com>
2023-09-25 14:51:53 +0530

    add uncaughtException ad unhandledRejection handlers

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-const app = require("./app");
+import app from './app';
+import logger from './lib/logger';
+
+const server = app.listen(3000, () => {
+  logger.info(`Listening to port ${3000}`);
+});
+
+const unexpectedErrorHandler = (error:Error) => {
+  logger.info(error);
+  process.exit(1);
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
