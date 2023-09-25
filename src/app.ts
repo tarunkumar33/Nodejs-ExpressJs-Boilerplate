2fe7f64f583b2cb5a685917fab5d29742e7a69f8
Tarun Kumar <tarun.p@technovert.com>
2023-09-25 15:06:41 +0530

    use helmet to secure express app by setting HTTP response headers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
+
+// set security HTTP response headers
+app.use(helmet());
+
+
 
 export default app;
